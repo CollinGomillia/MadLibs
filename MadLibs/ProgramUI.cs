5e13a9e5bc7d971ee516c5a94a274c9a375d8040
James Gilmer <gilmer.jamesandrew@gmail.com>
2021-12-08 20:28:31 -0500

    merging with github

--- MadLibs/ProgramUI.cs
+++ MadLibs/ProgramUI.cs
@@ -40,17 +40,13 @@ namespace MadLibs
                 switch (userInput)
                 {
                     case "1":
-                        Console.Clear();
                         ChristmasTreeStory();
                         break;
                     case "2":
-                        Console.Clear();
                         ChristmasBakingStory();
                         break;
                     case "3":
-                        Console.Clear();
                         GrinchStory();
-                        
                         break;
                     case "4":
                         NorthPoleStory();
@@ -74,82 +70,56 @@ namespace MadLibs
             //intake
             #region
             StoryWords input = new StoryWords();
-
-
+            List<string> verbwords = new List<string>();
 
 
             Console.WriteLine("Enter month");
             input.Month = Console.ReadLine();
 
-
             int verbCount = 5;
-            int verbDisplayCount = 5;
+            int displayCount = 5;
 
-            List<string> verbs = new List<string>();
             for (int i = 0; i < verbCount; i++)
             {
-                Console.WriteLine($"Please enter {verbDisplayCount} verbs");
+                Console.WriteLine($"please {displayCount} enter Enter a verb");
                 string userInput = Console.ReadLine();
-                verbs.Add(userInput);
-                verbDisplayCount -= 1;
+                verbwords.Add(userInput);
+                displayCount -= 1;
             }
-            input.Verbs = verbs;
+            input.Verbs = verbwords;
 
+            Console.WriteLine($"verb1 {input.Verbs[0]}");
 
-            // Console.WriteLine($"verb1 {input.Verbs[0]}"); is example for story
-
-            int placesCount = 2;
-            int placesDisplayCount = 2;
-           
-            List<string> places = new List<string>();
-            for (int i = 0; i < placesCount; i++)
-            {
-                Console.WriteLine($"Please enter {placesDisplayCount} places!");
-                string userInput = Console.ReadLine();
-                places.Add(userInput);
-                placesDisplayCount -= 1;
-            }
-            input.Places = places;
-
-
-            int adjectiveCount = 5;
-            int adjectiveDisplayCount = 5;
-            List<string> adjectives = new List<string>();
-            for (int i = 0; i < adjectiveCount; i++)
-            {
-                Console.WriteLine($"Please enter {adjectiveDisplayCount} adjectives!");
-                string userInput = Console.ReadLine();
-                adjectives.Add(userInput);
-                adjectiveDisplayCount -= 1;
-            }
-            input.Adjectives = adjectives;
-           
-           
-            int colorCount = 1;
-            int colorDisplayCount = 1;
-            List<string> color = new List<string>();
-            for (int i = 0; i < colorCount; i++)
-            {
-                Console.WriteLine($"Please enter {colorDisplayCount} color!");
-                string userInput = Console.ReadLine();
-                color.Add(userInput);
-                colorDisplayCount -= 1;
-            }
-            input.Color = color;
-
-            int nounCount = 3;
-            int nounDisplayCount = 3;
-            List<string> Nouns = new List<string>();
-            for (int i = 0; i < nounCount; i++)
-            {
-                Console.WriteLine($"Please enter {nounDisplayCount} nouns!");
-                string userInput = Console.ReadLine();
-                Nouns.Add(userInput);
-                nounDisplayCount -= 1;
-            }
-            input.Nouns = Nouns;
-
-
+            Console.WriteLine("Enter a place");
+            input.Places[0] = Console.ReadLine();
+            Console.WriteLine("Enter a adjective");
+            input.Adjectives[0] = Console.ReadLine();
+            Console.WriteLine("Enter a adjective");
+            input.Adjectives[1] = Console.ReadLine();
+            Console.WriteLine("Enter a place");
+            input.Places[1] = Console.ReadLine();
+            Console.WriteLine("Enter a verb");
+            input.Verbs[1] = Console.ReadLine();
+            Console.WriteLine("Enter a noun");
+            input.Nouns[0] = Console.ReadLine();
+            Console.WriteLine("Enter a verb");
+            input.Verbs[2] = Console.ReadLine();
+            Console.WriteLine("Enter a noun");
+            input.Nouns[1] = Console.ReadLine();
+            Console.WriteLine("Enter a adjective");
+            input.Adjectives[2] = Console.ReadLine();
+            Console.WriteLine("Enter a adjective");
+            input.Adjectives[3] = Console.ReadLine();
+            Console.WriteLine("Enter a color");
+            input.Color[0] = Console.ReadLine();
+            Console.WriteLine("Enter a adjective");
+            input.Adjectives[4] = Console.ReadLine();
+            Console.WriteLine("Enter a verb");
+            input.Verbs[3] = Console.ReadLine();
+            Console.WriteLine("Enter a verb");
+            input.Verbs[4] = Console.ReadLine();
+            Console.WriteLine("Enter a noun");
+            input.Nouns[2] = Console.ReadLine();
             Console.WriteLine("Enter a exclamation");
             input.Exclamation = Console.ReadLine();
             Console.WriteLine("Enter a holiday");
@@ -161,12 +131,7 @@ namespace MadLibs
 
             Console.WriteLine("Press any button to read your story");
             Console.ReadLine();
-            Console.WriteLine($"Every {input.Month} we {input.Verbs[0]} to a tree {input.Places[0]} far away. Not just any {input.Adjectives[0]} farm,\n" +
-                $" a {input.Adjectives[1]} tree {input.Places[0]}. My dad and I {input.Verbs[1]} onto the {input.Nouns[0]} to {input.Verbs[2]} for the perfect {input.Nouns[1]}. \n" +
-                $"Some people like them {input.Adjectives[2]} and {input.Adjectives[3]} and some like them {input.Color[0]} and fat. \n" +
-                $"We are searching for a tall and {input.Adjectives[4]} one! Over there!, I exclaimed, Dad it's over there! \n" +
-                $"Off we {input.Verbs[3]}, saw in hand to {input.Verbs[4]} this year's {input.Nouns[2]} down.\n" +
-                $" {input.Exclamation} it's {input.Holiday} finally!\n");
+            Console.WriteLine($"Every {input.Month} we {input.Verbs[0]} to a tree {input.Places[0]} far away. Not just any {input.Adjectives[0]} farm, a {input.Adjectives[1]} tree {input.Places[0]}. My dad and I {input.Verbs[1]} onto the  ");
 
 
 
@@ -261,106 +226,12 @@ namespace MadLibs
         }
         private void GrinchStory()
         {
-            StoryWords input = new StoryWords();
-            #region
-            int adjectiveCount = 2;
-            int adjectiveDisplayCount = 2;
-
-            List<string> adjectives = new List<string>();
-            for (int i = 0; i < adjectiveCount; i++)
-            {
-                Console.WriteLine($"Please enter {adjectiveDisplayCount} adjectives!");
-                string userInput = Console.ReadLine();
-                adjectives.Add(userInput);
-                adjectiveDisplayCount -= 1;
-            }
-            input.Adjectives = adjectives;
-
-            int colorCount = 2;
-            int colorDisplayCount = 2;
-
-            List<string> colors = new List<string>();
-            for (int i = 0; i < colorCount; i++)
-            {
-                Console.WriteLine($"Please enter {colorDisplayCount} colors!");
-                string userInput = Console.ReadLine();
-                colors.Add(userInput);
-                colorDisplayCount -= 1;
-            }
-            input.Color = colors;
-
-            Console.WriteLine("Enter a animal!");
-            input.Animal = Console.ReadLine();
-
-            int verbCount = 1;
-            int verbDisplayCount = 1;
-
-            List<string> verbs = new List<string>();
-            for (int i = 0; i < verbCount; i++)
-            {
-                Console.WriteLine($"Please enter {verbDisplayCount} verb!");
-                string userInput = Console.ReadLine();
-                verbs.Add(userInput);
-                verbDisplayCount -= 1;
-            }
-            input.Verbs = verbs;
-
-            int nounCount = 1;
-            int nounDisplayCount = 1;
-            List<string> Nouns = new List<string>();
-            for (int i = 0; i < nounCount; i++)
-            {
-                Console.WriteLine($"Please enter {nounDisplayCount} nouns!");
-                string userInput = Console.ReadLine();
-                Nouns.Add(userInput);
-                nounDisplayCount -= 1;
-            }
-            input.Nouns = Nouns;
-
-            int IngCount = 1;
-            int IngDisplayCount = 1;
-            List<string> IngVerb = new List<string>();
-            for (int i = 0; i < IngCount; i++)
-            {
-                Console.WriteLine($"Please enter {IngDisplayCount} verbs ending in ing!");
-                string userInput = Console.ReadLine();
-                IngVerb.Add(userInput);
-                IngDisplayCount -= 1;
-            }
-            input.IngVerb = IngVerb;
-
-            int PluralCount = 3;
-            int PluralDisplayCount = 3;
-            List<string> PluralNouns = new List<string>();
-            for (int i = 0; i < PluralCount; i++)
-            {
-                Console.WriteLine($"Please enter {PluralDisplayCount} plural nouns!");
-                string userInput = Console.ReadLine();
-                PluralNouns.Add(userInput);
-                PluralDisplayCount -= 1;
-            }
-            input.PluralNouns = PluralNouns;
-
-            Console.WriteLine("Press any button to read your story");
-            Console.ReadLine();
-            Console.WriteLine($"The Grinch is a(n) {input.Adjectives[0]} {input.Color[0]} creature with {input.Color[1]} eyes who does not like Christmas cheer.\n" +
-                $" When he sees people celebrating Christmas, it makes him {input.Adjectives[1]}.\n" +
-                $" He and his {input.Animal} Max try to {input.Verbs[0]} Christmas for people in the town of {input.Nouns[0]}-ville \n" +
-                $" by their Christmas {input.PluralNouns[0]}. When the people \n" +
-                $" still have Christmas cheer, even without {input.PluralNouns[1]}, the Grinch decides to return the {input.PluralNouns[2]} and celebrates \n" +
-                $" Christmas after all. ");
-
-            Console.ReadLine();
-            Console.Clear();
-
-            #endregion
         }
         private void NorthPoleStory()
         {
-
         }
 
 
-    }
 
+    }
 }
